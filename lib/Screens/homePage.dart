2b2364356de9b36ffa64bf3d78eadbf0336f4f4d
shubham189301011 <shubham.189301011@muj.manipal.edu>
2021-01-23 22:49:49 +0530

    Chat app hai

---
+++ lib/Screens/homePage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:chatapp/Screens/chatPage.dart';
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text("Chats"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts_rounded),
+            title: Text("Contacts"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            title: Text("Profile"),
+          ),
+        ],
+      ),
+    );
+  }
+}
